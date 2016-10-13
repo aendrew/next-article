c7a1d68b46a106ef050e983d76f0791d21582761
Andrew Georgiou <andrew.georgiou@ft.com>
2016-10-13 16:22:47 +0100

    Ads; if the last element in the article body is not a paragraph then we no longer wish to insert an ad

--- server/transforms/inline-ad.js
+++ server/transforms/inline-ad.js
@@ -31,8 +31,9 @@ module.exports = function ($, flags) {
 
 	if(flags.threeAdProposition && pars.length <= 3) {
 		//If article shorter than three pars, insert an ad right at the end
-		if ($('*').last()[0].name === 'p'){
-			$('*').last().after(midAd);
+		let lastElement = $('*').last();
+		if (lastElement[0].name === 'p'){
+			lastElement.after(midAd);
 		}
 	} else {
 
