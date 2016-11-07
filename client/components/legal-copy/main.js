3f46b250ebc04e4588df078c0907e845647628c9
phamann <patrickhamann@gmail.com>
2016-11-07 11:11:37 +0000

    Refactor legal-copy component to mutate clipboardData with legal message

--- client/components/legal-copy/main.js
+++ client/components/legal-copy/main.js
@@ -1,8 +1,30 @@
-module.exports = function () {
+'use strict';
+
+function supportsClipboard (e) { return e.clipboardData || window.clipboardData; }
+function hasEventClipboardSupport (e) { return e.clipboardData && !!e.clipboardData.setData }
+
+module.exports = function (flags) {
 	const articleBody = document.querySelector('[data-legal-copy]');
 
+	if(!articleBody) { return }
+	if (!flags.get('articleCopyLegalNotice')) { return }
+
+	const tsAndCsLink = 'http://help.ft.com/legal/ft-com-terms-and-conditions/';
+	const copyrightLink = 'http://help.ft.com/legal/copyright-policy/';
+	const supportEmail = 'ftsales.support@ft.com';
+
+	const msgText = `High quality global journalism requires investment. Please share this article with others using the link below, do not cut & paste the article. See our Ts&Cs and Copyright Policy for more detail. Email ${supportEmail} to buy additional rights.`;
+	const msgHtml = `High quality global journalism requires investment. Please share this article with others using the link below, do not cut & paste the article. See our <a href="${tsAndCsLink}" target="_blank">Ts&Cs</a> and <a href="${copyrightLink}" target="_blank">Copyright</a> Policy for more detail. Email <a href="mailto:${supportEmail}">${supportEmail}</a> to buy additional rights.`;
+
 	articleBody.addEventListener('copy', (e) => {
-		e.clipboardData.setData('text/plain', 'naughty');
+		if(!supportsClipboard(e)) { return }
+
+		if(hasEventClipboardSupport(e)) {
+			e.clipboardData.setData('text/html', msgHtml);
+		} else {
+			window.clipboardData.setData('Text', msgText);
+		}
+
 		e.preventDefault();
 	});
 }
