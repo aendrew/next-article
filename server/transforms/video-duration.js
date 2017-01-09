e8e4d44403d4dbd2f8b6d2c076da74c3d5dc0050
Matt Hinchliffe <i-like-robots@users.noreply.github.com>
2017-01-09 15:22:32 +0000

    Delimit video duration with a colon

--- server/transforms/video-duration.js
+++ server/transforms/video-duration.js
@@ -3,7 +3,7 @@ module.exports = duration => {
 		const minutes = Math.floor(duration / 60000);
 		const seconds = Math.round(duration / 1000) - minutes * 60;
 
-		duration = `${minutes}.${seconds < 10 ? '0' : ''}${seconds}min`;
+		duration = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}min`;
 	}
 
 	return duration;
