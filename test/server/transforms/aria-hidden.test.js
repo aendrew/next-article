101e1e51be42d1e7ee0e678ae5fb605c899fe4d1
Keith Rogers <git@keithrogers.eu>
2017-01-16 11:46:52 +0000

    Add a test for teaser-specific related content, just in case

--- test/server/transforms/aria-hidden.test.js
+++ test/server/transforms/aria-hidden.test.js
@@ -27,4 +27,14 @@ describe('Adding aria-hidden to items that break main content flow, in order to 
 			'<aside class="n-content-related-box p402_hide" role="complementary" data-trackable="related-box" aria-hidden="true"><div class="n-content-related-box__headline">More</div><div class="n-content-related-box__content"><p><a href="http://rankings.ft.com/businessschoolrankings/european-business-school-rankings-2016" data-trackable="link">Interactive table</a><br>View our ranking of the top European business schools</p><p><a href="/content/46a27db8-afe4-11e6-9c37-5787335499a0" data-trackable="link">Methodology and key</a><br>Ranking measures the quality and breadth of programmes</p><p><a href="/content/5477e424-afde-11e6-9c37-5787335499a0" data-trackable="link">Meet the dean</a><br>Bocconi University’s Gianmario Verona</p></div></aside>'
 		);
 	});
+
+	it('should add `aria-hidden="true"` to teaserified related-content boxes', () => {
+		const $ = cheerio.load('<aside class="n-content-related-box n-content-related-box--no-border p402_hide" role="complementary" data-trackable="related-box"><div class="standalone-teaser-heading">Read more</div><div class="o-teaser o-teaser--large" data-o-component="o-teaser" data-trackable="teaser"><div class="o-teaser__content"><div class="o-teaser__meta"></div><h3 class="o-teaser__heading"><a href="/content/3ba3ebe8-afdc-11e6-9c37-5787335499a0" data-trackable="main-link">MBA graduate makes a material difference in sustainable fashion</a></h3><p class="o-teaser__standfirst">St Gallen programme gave Megan McGill the skills to tailor a new career</p><div class="o-teaser__timestamp"><time data-o-component="o-date" class="o-date" data-o-date-format="time-ago-limit-4-hours" datetime="" title="January 16, 2017 11:18 am" data-o-date-js="" aria-label=""></time></div></div></div></aside>',
+			{ decodeEntities: false }
+		);
+		const transformed$ = ariaHiddenTransform($);
+
+		transformed$.html().should.equal('<aside class="n-content-related-box n-content-related-box--no-border p402_hide" role="complementary" data-trackable="related-box" aria-hidden="true"><div class="standalone-teaser-heading">Read more</div><div class="o-teaser o-teaser--large" data-o-component="o-teaser" data-trackable="teaser"><div class="o-teaser__content"><div class="o-teaser__meta"></div><h3 class="o-teaser__heading"><a href="/content/3ba3ebe8-afdc-11e6-9c37-5787335499a0" data-trackable="main-link">MBA graduate makes a material difference in sustainable fashion</a></h3><p class="o-teaser__standfirst">St Gallen programme gave Megan McGill the skills to tailor a new career</p><div class="o-teaser__timestamp"><time data-o-component="o-date" class="o-date" data-o-date-format="time-ago-limit-4-hours" datetime="" title="January 16, 2017 11:18 am" data-o-date-js="" aria-label=""></time></div></div></div></aside>'
+		);
+	});
 });
