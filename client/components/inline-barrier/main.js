ee955b3f287e5e9a448f9524c22808130d22385a
Keith Rogers <git@keithrogers.eu>
2017-02-12 22:20:40 +0000

    Modify the PSP title text, horribly
    Note: I do not like this and am sorry for doing it. Do ask if you want
    to know my story of why it had to be done. AMA

--- client/components/inline-barrier/main.js
+++ client/components/inline-barrier/main.js
@@ -9,8 +9,26 @@ const populateWithPsp = (el) => fetch('/products?fragment=true&inline=true&narro
 	.then(response => {
 		if (response.ok) {
 			return response.text().then(html => {
-				el.insertAdjacentHTML('beforeend', html);
-				el.classList.remove('n-util-visually-hidden');
+				const parsedHtml = new DOMParser().parseFromString(html, 'text/html');
+				const pspEl = parsedHtml.querySelector('.barrier');
+				//Remove duplicate ID so pa11y passes
+				pspEl && pspEl.removeAttribute('id');
+				const pspTitle = pspEl && pspEl.querySelector('.barrier-grid__heading-pretext');
+
+				if (pspTitle) {
+					// Horrible hacks to modify the PSP title text, for a slightly dirty MVT
+					const titleContainer = pspTitle.parentElement;
+					const nonH1Title = document.createElement('p');
+					nonH1Title.className = 'barrier-grid__heading barrier-grid__heading-pretext';
+					nonH1Title.innerHTML = 'Want to read more?';
+					const sub = document.createElement('p');
+					sub.innerHTML = '<span class="barrier-grid__article-title">Subscribe by choosing a package below:</span>';
+					titleContainer.insertAdjacentElement('beforeend', nonH1Title);
+					titleContainer.insertAdjacentElement('beforeend', sub);
+					pspTitle.remove();
+				}
+
+				el.insertAdjacentElement('beforeend', pspEl);
 			});
 		}
 	});
