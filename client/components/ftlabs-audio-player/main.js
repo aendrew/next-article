f4af756e6949e0ac5aee57705a6abd4343275ba0
Sean Tracey <seanmtracey@gmail.com>
2017-02-15 15:12:17 +0000

    Track the audio events with o-tracking. (#1861)
    Linting

--- client/components/ftlabs-audio-player/main.js
+++ client/components/ftlabs-audio-player/main.js
@@ -5,6 +5,25 @@ export default () => {
 	const holder = document.querySelector('.ftlabsaudioplayerholder');
 	const audioElement = document.querySelector('audio.ftlabs-audio-player');
 
+	function logPlayerInteractions (interaction){
+		document.body.dispatchEvent( new CustomEvent('oTracking.event',
+			{
+				detail: {
+					action: `ftlabsaudio-${interaction}`,
+					category: 'audio',
+					contentID: articleUUID,
+					position: audioElement.currentTime
+				}, bubbles: true
+			}
+		));
+
+	};
+
+	audioElement.addEventListener('play', () => { logPlayerInteractions('play') }, false);
+	audioElement.addEventListener('pause', () => { logPlayerInteractions('pause') }, false);
+	audioElement.addEventListener('seeked', () => { logPlayerInteractions('seek') }, false);
+	audioElement.addEventListener('ended', () => { logPlayerInteractions('ended') }, false);
+
 	fetch(`https://ftlabs-audio-available.herokuapp.com/check/${articleUUID}`)
 		.then(res => {
 			if(res.ok){
