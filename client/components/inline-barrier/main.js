88bec95b0f98976ae41b84e8fe47bfc4bfca8a30
Keith Rogers <git@keithrogers.eu>
2017-02-26 20:57:11 +0000

    Wipe over core inline-barrier with enhanced when available
    Simplifies things a bit. Also separates the enhanced block into its own
    partial, which is now a script template. Sorry for the annoyingly
    multi-functional commit :-/

--- client/components/inline-barrier/main.js
+++ client/components/inline-barrier/main.js
@@ -88,15 +88,23 @@ const populateWithTrial = (el) => fetch(`https://next-signup-api.ft.com/offer/41
 	})
 	.then(json => json.data)
 	.then(({offer}) => {
-		//FIXME: Populate copy with new offer data
-		const trialPriceables = [...document.querySelectorAll('.js-barrier-trial-price')];
-		const postTrialPriceables = [...document.querySelectorAll('.js-barrier-trial-after-price')];
+		const coreWrapper = el.querySelector('.inline-barrier__content');
+		const enhancedTrialTemplate = document.querySelector('#js-barrier-trial');
+
+		if (!el || !enhancedTrialTemplate || !coreWrapper) { return; }
+
+		coreWrapper.innerHTML = enhancedTrialTemplate.innerHTML;
+		const trialPriceables = [...coreWrapper.querySelectorAll('.js-barrier-trial-price')];
+		const postTrialPriceables = [...coreWrapper.querySelectorAll('.js-barrier-trial-after-price')];
+
 		const trialOffer = offer.charges.find(charge => charge.billing_period === 'trial');
 		//FIXME: make sure this is the right object: why different amounts from psp page?
 		const postTrialOffer = offer.charges.find(charge => charge.billing_period === 'monthly');
 		//TODO: investigate localization of symbol/value ordering
+
 		trialPriceables.forEach(priceable => priceable.innerHTML = `${trialOffer.amount.symbol}${trialOffer.amount.value.replace('.00', '')}`);
 		postTrialPriceables.forEach(priceable => priceable.innerHTML = `${postTrialOffer.display_amount.weekly.symbol}${postTrialOffer.display_amount.weekly.value}`);
+
 		el.classList.remove('inline-barrier--no-prices');
 		el.classList.add('inline-barrier--has-prices');
 
