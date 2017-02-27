9d6ae752a3917937fbd33047002457daaff2796b
Keith Rogers <git@keithrogers.eu>
2017-02-27 09:04:43 +0000

    Separate barrier population into variants, share broadcast call
    Worst commit ever

---
+++ client/components/inline-barrier/broadcast-opportunity.js
@@ -0,0 +1,26 @@
+import { broadcast } from 'n-ui/utils';
+
+module.exports = (subtype) => {
+	const appNameEl = document.querySelector('[data-next-app]');
+	const appVersionEl = document.querySelector('[data-next-version]');
+	const context = {
+		product: 'next',
+		app: appNameEl && appNameEl.getAttribute('data-next-app'),
+		appVersion: appVersionEl && appVersionEl.getAttribute('data-next-version')
+	};
+	const acquisitionContextEls = document.querySelectorAll('[data-acquisition-context]');
+	const offersEls = document.querySelectorAll('[data-offer-id]');
+
+	broadcast('oTracking.event', Object.assign({
+		category: 'barrier',
+		action: 'view',
+		opportunity: {
+			type: 'barrier',
+			subtype
+		},
+		type: 'barrier',
+		acquisitionContext: Array.from(acquisitionContextEls).map(e => e.getAttribute('data-acquisition-context')),
+		offers: Array.from(offersEls).map(e => e.getAttribute('data-offer-id'))
+	}, context))
+};
+
