44f3c72ce3aed6041e6764394d2cb85b1af99878
Kavanagh, Luke <ltkavanagh@gmail.com>
2017-03-02 09:42:01 +0000

    Remove experimental content switching

--- server/controllers/negotiation.js
+++ server/controllers/negotiation.js
@@ -1,8 +1,6 @@
 const fetchres = require('fetchres');
 const logger = require('@financial-times/n-logger').default;
-const api = (process.env.EXPERIMENTAL_CONTENT_SOURCE && process.env.NODE_ENV !== 'production')
-	? require('../lib/experimental-content-client')
-	: require('next-ft-api-client');
+const api = require('next-ft-api-client');
 const interactivePoller = require('../lib/ig-poller');
 const shellpromise = require('shellpromise');
 const getOnwardJourneyArticles = require('./article-helpers/onward-journey');
