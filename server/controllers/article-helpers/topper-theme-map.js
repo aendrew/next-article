02f1b06a393967ea342d0ffd6bfac0ad6e74f349
Gabrielle von Koss <gabivk@gmail.com>
2017-03-08 15:09:57 +0000

    mergeflix

--- server/controllers/article-helpers/topper-theme-map.js
+++ server/controllers/article-helpers/topper-theme-map.js
@@ -27,41 +27,27 @@ const getTopperSettings = (content, flags) => {
 		const hasImage = themeOrLayout !== 'full-bleed-text';
 		const backgroundColour = themeOrLayout === 'full-bleed-offset' ? 'pink' : (content.topper.backgroundColour || 'pink');
 		const topperProperties = allProperties(themeOrLayout, template, backgroundColour, hasImage, topperOrLeadImages);
-		const extraProperty = {themeImageRatio: themeImageRatio[themeOrLayout]};
 
-		return Object.assign(topperProperties, extraProperty);
-
+		return Object.assign(topperProperties, {themeImageRatio: themeImageRatio[themeOrLayout]});
 	} else if (flags.contentPackages && content.containedIn && content.containedIn.length) {
-<<<<<<< HEAD
 		return allProperties('full-bleed-offset', 'offset', 'slate', true, topperOrLeadImages)
-
-=======
-		return {
-			theme: 'full-bleed-offset',
-			template: 'offset',
-			backgroundColour: 'pink', //TODO make the background work
-			myFtButtonVariant: myFtButtonVariant('pink')
-		};
->>>>>>> 87ab56d9c4cc547334f45fe232b1e66a61bc81b7
 	} else if(content.designGenre) {
 		const topperProperties = basicProperties('branded', 'basic', 'warm-1');
 
 		return Object.assign(topperProperties, { myFtButtonVariant: 'standard' });
-
 	} else {
 		return basicProperties(null, 'basic', 'pink');
 	}
 };
 
-
 const allProperties = (layout, template, backgroundColour, includesImage, images) => {
 	const addOn = {
-			includesImage,
-			images
+		includesImage,
+		images
 	}
-
 	return Object.assign(basicProperties(layout, template, backgroundColour), addOn)
 }
+
 const basicProperties = (layout, template, backgroundColour) => {
 	return {
 		layout,
