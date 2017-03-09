5f42d066d014cc6cf25bcc59ce840f5882864899
Arjun Gadhia <arjungadhia@gmail.com>
2017-03-09 15:17:29 +0000

    Styles for series hub toppers

--- server/controllers/article-helpers/topper-theme-map.js
+++ server/controllers/article-helpers/topper-theme-map.js
@@ -15,26 +15,35 @@ const getTopperSettings = (content, flags) => {
 	let themeOrLayout;
 	let topperOrLeadImages;
 
-	if(content.topper) {
-		//TODO: change to layout only when migration is complete
-		themeOrLayout = content.topper.layout || content.topper.theme;
-		//TODO: change to leadImages only when migration is complete
-		topperOrLeadImages = content.leadImages || content.topper.images;
-	}
+	content.topper = content.topper || {};
 
-	if(flags.articleTopper && content.topper && themeOrLayout && themeImageRatio.hasOwnProperty(themeOrLayout)) {
+	//TODO: change to layout only when migration is complete
+	themeOrLayout = content.topper.layout || content.topper.theme;
+	//TODO: change to leadImages only when migration is complete
+	topperOrLeadImages = content.leadImages || content.topper.images;
+	
+	//Articles within a package get a slate offset topper if the package has the 'extra' theme
+	if (flags.contentPackages && content.containedIn && content.containedIn.length) {
+		return allProperties('full-bleed-offset', 'offset', 'slate', true, topperOrLeadImages);
+	//otherwise use the editorially selected topper if it exists
+	} else if (flags.contentPackages && content.type === 'package' && content.design && content.design.theme === 'extra') {
+		return allProperties('split-text-left', 'themed', 'slate', true, topperOrLeadImages);
+	} else if (flags.contentPackages && content.type === 'package' && content.package.design && content.package.design.theme === 'special-report') {
+		return allProperties('split-text-left', 'themed', 'claret', true, topperOrLeadImages);
+	} else if(flags.articleTopper && content.topper && themeOrLayout && themeImageRatio.hasOwnProperty(themeOrLayout)) {
 		const template = themeOrLayout === 'full-bleed-offset' ? 'offset' : 'themed';
 		const hasImage = themeOrLayout !== 'full-bleed-text';
 		const backgroundColour = themeOrLayout === 'full-bleed-offset' ? 'pink' : (content.topper.backgroundColour || 'pink');
 		const topperProperties = allProperties(themeOrLayout, template, backgroundColour, hasImage, topperOrLeadImages);
 
-		return Object.assign(topperProperties, {themeImageRatio: themeImageRatio[themeOrLayout]});
-	} else if (flags.contentPackages && content.containedIn && content.containedIn.length) {
-		return allProperties('full-bleed-offset', 'offset', 'slate', true, topperOrLeadImages)
+		return Object.assign(topperProperties);
+
+		//Branded regular toppers
 	} else if(content.designGenre) {
 		const topperProperties = basicProperties('branded', 'basic', 'warm-1');
-
 		return Object.assign(topperProperties, { myFtButtonVariant: 'standard' });
+
+	//everything else gets a regular topper
 	} else {
 		return basicProperties(null, 'basic', 'pink');
 	}
@@ -54,6 +63,7 @@ const basicProperties = (layout, template, backgroundColour) => {
 		template,
 		backgroundColour,
 		myFtButtonVariant: myFtButtonVariant(backgroundColour),
+		themeImageRatio: themeImageRatio[layout]
 	};
 }
 
