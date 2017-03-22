4d9d2e7347a3b6eec5cedce87a3748f486977110
Arjun Gadhia <arjungadhia@gmail.com>
2017-03-22 14:18:12 +0000

    PLP topper tagging

--- server/controllers/article-helpers/get-package-brand.js
+++ server/controllers/article-helpers/get-package-brand.js
@@ -2,15 +2,11 @@ const SPECIAL_REPORT = 'MjBkZDZhODAtYTQzOS00YmM1LWI4ZTItN2VjM2QwZmI0NTMw-QnJhbmR
 const FT_SERIES = 'ZjIwNzVmMDQtNzU3OC00NDY4LTg3MmItNGUyYmQ5ZjY3NGY3-QnJhbmRz';
 
 module.exports = (metadata) => {
-	if(metadata.find(tag => tag.idV1 === SPECIAL_REPORT)) {
-		return {
-			full: 'Special Report',
-			inSentence: 'Special Report'
-		};
-	} else if (metadata.find(tag => tag.idV1 === FT_SERIES)) {
-		return {
-			full: 'FT Series',
-			inSentence: 'series'
-		};
+	const brand = metadata.find(tag => [SPECIAL_REPORT, FT_SERIES].includes(tag.idV1));
+
+	if(brand) {
+		return Object.assign({
+			inSentence: brand.idV1 === FT_SERIES ? 'series' : brand.prefLabel
+		}, brand);
 	}
 }
