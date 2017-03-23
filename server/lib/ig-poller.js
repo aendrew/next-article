5323bb198d098f93078949e8ec02ce0f512343ca
Kavanagh, Luke <ltkavanagh@gmail.com>
2017-03-23 09:44:38 +0000

    Improve IG poller
    The is mainly for performance reasons: it speeds up the IG lookup and
    reduces the memory consumed by holding the list.

--- server/lib/ig-poller.js
+++ server/lib/ig-poller.js
@@ -2,7 +2,13 @@ const Poller = require('ft-poller');
 
 const poller = new Poller({
 	url: 'https://bertha.ig.ft.com/view/publish/gss/1Rlx_BfkwCpE8KBJZVK_p5gO61HZF7fRckQige6Hwbl8/mappings',
-	defaultData: []
+	defaultData: new Map(),
+	parseData: rows => (rows || []).reduce((map, row) => {
+		if (row.articleuuid && row.interactiveurl && row.displaytype === 'redirect') {
+			map.set(row.articleuuid, row.interactiveurl);
+		}
+		return map;
+	}, new Map())
 });
 
 function start () {
@@ -13,4 +19,10 @@ function getData () {
 	return poller.getData();
 }
 
-module.exports = { start, getData };
+function lookup (id) {
+	const map = poller.getData();
+	if (!map) return;
+	return map.get(id);
+}
+
+module.exports = { start, getData, lookup };
