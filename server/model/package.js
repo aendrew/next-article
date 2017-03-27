a95b1fb745d6bcdecbc9605da87bd393176599f8
Kavanagh, Luke <ltkavanagh@gmail.com>
2017-03-27 18:35:26 +0100

    Woops

--- server/model/package.js
+++ server/model/package.js
@@ -68,11 +68,8 @@ module.exports = function decoratePackage (req, res, payload, flags) {
 
 		content.helpers = content.helpers || {};
 		content.helpers.gt = (a, b) => a > b;
-<<<<<<< HEAD
-=======
 
 		const inverseTheme = content.design.theme === 'extra' || content.design.theme === 'extra-wide';
->>>>>>> 99af18f... Lint
 
 		content.helpers.bigTeasers = function (options) {
 			const items = numChildren > maxBigTeasers ? content.contains.slice(0, minBigTeasers) : content.contains;
@@ -89,7 +86,8 @@ module.exports = function decoratePackage (req, res, payload, flags) {
 			if (items.length) {
 				return options.fn({
 					title,
-					items
+					items,
+					teaserMods: ['hero', 'centre', 'stretched', inverseTheme && 'inverse']
 				});
 			}
 			return options.inverse(this);
@@ -105,7 +103,8 @@ module.exports = function decoratePackage (req, res, payload, flags) {
 				}
 				return options.fn({
 					title,
-					items: numChildren > maxBigTeasers ? content.contains.slice(minBigTeasers) : []
+					items: numChildren > maxBigTeasers ? content.contains.slice(minBigTeasers) : [],
+					teaserMods: ['small', inverseTheme && 'inverse']
 				});
 			}
 			return options.inverse(this);
